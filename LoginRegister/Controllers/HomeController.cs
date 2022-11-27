3f117798b835aa56b7f13b3677587a363b0a508e
unknown <ngobesenduduzo080@gmail.com>
2022-11-27 23:50:52 +0200

    initial commit

---
+++ LoginRegister/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegister.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LoginRegContext lgContext = new LoginRegContext();
+        public ActionResult Index()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+           
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = lgContext.Users.FirstOrDefault(s => s.email == user.email);
+                if (check == null)
+                {
+                    user.password = GetMD5(user.password);
+                    lgContext.Configuration.ValidateOnSaveEnabled = false;
+                    lgContext.Users.Add(user);
+
+                    lgContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var f_password = GetMD5(password);
+                var data = lgContext.Users.Where(s => s.email.Equals(email) && s.password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    Session["fullName"] = data.FirstOrDefault().fullName;
+                    Session["email"] = data.FirstOrDefault().email;
+                    Session["UserID"] = data.FirstOrDefault().UserId;
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
+
+
+
+    }
+}
